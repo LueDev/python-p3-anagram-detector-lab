814ef16cc9119a7e87617ec94eaa3341c6861394
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-03-29 13:47:24 -0400

    Very tricky. The list comprehension for this was correct but the func declaration previously used *args and that created an issue. For the expression, used word for word in arr rather than print(word) for word in arr. This fixed some bugs too. Careful with the output requested and whether its a return or a print

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
 # your code goes here!
+
+class Anagram:
+    
+    def __init__(self, wordToCheck):
+        self.wordToCheck = wordToCheck
+        
+    
+    @property
+    def wordToCheck(self):
+        return self._wordToCheck
+    
+    @wordToCheck.setter
+    def wordToCheck(self, word):
+        self._wordToCheck = word
+    
+    def match(self, arr):
+        return [word for word in arr if all(char in word for char in self._wordToCheck)] if len(arr) > 0 else []
+
+
+if __name__ == "__main__":
+    listen = Anagram("listen")
+    # print(listen.wordToCheck)
+    listen.match(['enlists', 'google', 'inlets', 'banana', 'listen'])
